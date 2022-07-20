44601432389e5f07bbed77afa125b0bf9ad516e0
frosty <frostdxgdafaq@gmail.com>
2022-07-20 11:54:40 +0530

    added wep app

---
+++ Moshan/mavenproject1/src/main/java/com/mycompany/mavenproject1/resources/JakartaEE9Resource.java
@@ -0,0 +1,20 @@
+package com.mycompany.mavenproject1.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee9")
+public class JakartaEE9Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
